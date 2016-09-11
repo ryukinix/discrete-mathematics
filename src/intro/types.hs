5c6aa5fe5cf0d5333905d439e167979fa56c01d3
Manoel Vilela <manoel_vilela@engineer.com>
2016-09-11 06:34:43 -0300

    Add just the basic number types of Haskell

---
+++ src/intro/types.hs
@@ -0,0 +1,18 @@
+{--
+   Just a simple and common types on Haskell
+--}
+
+
+
+-- Number types
+int :: Int
+integer :: Integer
+float :: Float
+rational :: Rational
+bool :: Bool
+
+int = 1
+integer = 2^200
+float = 3/4
+rational = 3/4
+bool = True
