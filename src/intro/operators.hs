f6d6bbc2e350968eaef2df298f369b65f86bb1e8
Manoel Vilela <manoel_vilela@engineer.com>
2016-09-11 14:57:54 -0300

    Add common operators of haskell and declarations

---
+++ src/intro/operators.hs
@@ -0,0 +1,34 @@
+{--
+   The mostly commons operators for basic logic in Haskell
+--}
+
+
+-- the declaration of function with multiple params
+-- is done that way because the currying. Reducing
+-- functions to just a dataflow. So do this is wrong:
+-- and :: (Bool, Bool) -> Bool
+
+-- btw, makes some sense on a structured language.
+-- HASKELL IS NOT A STRUCTURED LANGUAGE!
+
+-- :: DECLARATIONS
+eq :: Integer -> Integer -> Bool
+greater :: Integer -> Integer -> Bool
+greater'eq :: Integer -> Integer -> Bool
+less :: Integer -> Integer -> Bool
+less'eq :: Integer -> Integer -> Bool
+not'equal :: Bool -> Bool -> Bool
+and :: Bool -> Bool -> Bool
+or :: Bool -> Bool -> Bool
+not' :: Bool -> Bool
+
+-- :: DEFINITIONS
+eq x y = x == y
+greater x y = x > y
+greater'eq x y = x >= y
+less x y = x < y
+less'eq x y = x <= y
+not'equal x y = x /= y
+and x y = x && y
+or x y = x || y
+not' x = not x
