ba62105fb25874c5b3665b7cc47489d4386d1434
Manoel Vilela <manoel_vilela@engineer.com>
2016-09-10 22:22:34 -0300

    First steps into the beauty world of mathematics

---
+++ README.md
@@ -0,0 +1,16 @@
+# Abstract
+
+This project is to help computer science create a heavy relation with your journey and mathematics, using discrete mathematics to understand the fundamentals
+of computer science using Haskell.
+
+# Mission
+
+The main target of this repository is use git to save all the solutions of book exercises explaining really easilly the mathematics concepts envolved
+as well the Haskell too.
+
+# Authors
+
+* Manoel Machado
+
+# License
+MIT
---
+++ solutions/intro/functions.hs
@@ -0,0 +1,28 @@
+{--
+
+    This file explain the basic function to do:
+
+    * type declaration
+    * pattern matching
+    * function definition
+    * function composition
+
+--}
+
+-- :: factorial function
+fat :: Integer -> Integer
+fat 0 = 1
+fat n | n > 0 = n * fat (n - 1)
+
+-- :: fibonacci function
+
+-- | domain and contra-domain declaration
+fib :: Integer -> Integer
+-- | pattern matching
+fib n | n <= 1 = 1
+-- | formal definition of fib function
+fib n | n > 1 = fib(n - 1) + fib(n - 2)
+
+-- | function composition
+-- is just like fatfib = fat(fib(x))
+fatfib = fat . fib
