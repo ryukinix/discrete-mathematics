b25f30ce8862cbbf680beaec55a221fc9e2b4a01
Manoel Vilela <manoel_vilela@engineer.com>
2017-03-17 23:40:08 -0300

    Update types.hs: give a brief description of the types

--- src/01-introduction-to-haskell/types.hs
+++ src/01-introduction-to-haskell/types.hs
@@ -1,5 +1,15 @@
 {--
    Just a simple and common types on Haskell
+
+Int stands for integer with fixed precision
+Integer stands for a integer with arbitrary precision (BigNum)
+Float is real floating point with single precision
+Double is a real floating point with double the precision
+Bool is a boolean type. It can have only two values: True and False.
+Char represents a character. It's denoted by single quotes.
+String is a list of characters.
+IO handles Input and Output
+
 --}
 
 
@@ -17,7 +27,7 @@ bool :: Bool
 main :: IO ()
 
 int = 1
-integer = 2^200
+integer = 2 ^ 100
 float = 3/4
 rational = 3/4
 bool = True
