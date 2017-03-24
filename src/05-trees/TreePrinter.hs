0799d4e6f5448ab02c3633868c9f8041e971e9d7
Manoel Vilela <manoel_vilela@engineer.com>
2017-03-23 21:16:00 -0300

    Remove the cancer code from TreePrinter
    Keep it simple. The other pretty printer is messy.

--- src/05-trees/TreePrinter.hs
+++ src/05-trees/TreePrinter.hs
@@ -1,27 +1,7 @@
-import Data.Function (on)
-import Data.List (sortBy, groupBy, intercalate)
+module TreePrinter
+  (prettyTree) where
+
 import Notes (BinTree(Node, Leaf))
-
-
-walkTree :: BinTree a -> [(Int, a)]
-walkTree Leaf = []
-walkTree root@(Node a l r) = let h = height root
-                                 left = walkTree l
-                                 right = walkTree r
-                             in [(h,a)] ++ left ++ right
-
-treeList :: BinTree b -> [(Int,[b])]
-treeList t = clean $ groups $ walkTree t
-  where groups = groupBy ((==) `on` fst ) . sortBy (compare `on` fst)
-        f xs = (fst (head xs), map snd xs)
-        clean = map f
-
-joinLine :: (Show a) => [a] -> String
-joinLine xs = intercalate " " $ map show xs
-
-muchPrettyTree :: Show a => BinTree a -> String
-muchPrettyTree t = unlines .  map parserLine $ treeList t
-  where parserLine (d,xs) = replicate (d+2) ' ' ++ joinLine xs
 
 prettyTree :: (Show a) => BinTree a -> Int -> String
 prettyTree Leaf _ = ""
@@ -29,14 +9,5 @@ prettyTree (Node a left right) d = (replicate d ' ') ++ "=> " ++ show a ++ "\n"
                                    ++ lookBranch left ++ lookBranch right
   where lookBranch b = prettyTree b (d + 3)
 
-height :: BinTree a -> Int
-height Leaf = 0
-height (Node _ l r) = 1 + max (height l) (height r)
-
 instance (Show a) => Show (BinTree a) where
   show t = prettyTree t 0
-
-testTree = Node 10 (Node 12 (Node 13 Leaf Leaf) (Node 14 Leaf Leaf)) (Node 11 Leaf Leaf)
-
-main :: IO()
-main = print $ testTree
